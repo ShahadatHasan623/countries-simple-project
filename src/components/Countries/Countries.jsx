b930b7736fabef16ebcaaabb0badca35b1c85b53
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-02-26 16:49:12 +0600

    added conditional rendering

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,17 +1,20 @@
-import React, { use } from 'react';
-import Country from '../Country/Country';
+import React, { use } from "react";
+import Country from "../Country/Country";
+import './Countries.css'
 
-const Countries = ({countriesPromise}) => {
-    const countryData =use(countriesPromise)
-    const countries = countryData.countries
-    return (
-        <div>
-            <h1>In the countries :{countries.length}</h1>
-            {
-                countries.map(country=><Country country={country}></Country>)
-            }
-        </div>
-    );
+const Countries = ({ countriesPromise }) => {
+  const countryData = use(countriesPromise);
+  const countries = countryData.countries;
+  return (
+    <div>
+      <h1>In the countries :{countries.length}</h1>
+      <div className="gird-layout">
+        {countries.map((country) => (
+          <Country country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Country = ({country}) => {
-    console.log(country);
-    const {name,flags,population,region}=country
+    const {name,flags,population,region,area}=country
+
+    const [visited,setVisited]=useState(false)
+
+    const handleVisited =()=>{
+        if(visited){
+            setVisited(false)
+        }
+        else{
+            setVisited(true)
+        }
+    }
     return (
-        <div>
+        <div className={`border ${visited && `visited-bg`}`} >
             <img src={flags.flags.png} alt={flags.flags.alt} />
             <h3>Name : {name.common}</h3>
             <p>Population : {population.population}</p>
             <p>Region : {region.region}</p>
+            <p>Area :{area.area} {area.area > 500000 ? 'Big Country': 'Small Country'}</p>
+            <button onClick={handleVisited}>{visited ? "visited" : "Not Visited"}</button>
         </div>
     );
 };
